66855273dafb686e63ccd71ab3fc2050b421cb76
Marcin Szymanek <szymanek.m@gmail.com>
2021-03-07 00:15:34 +0000

    finished holidays logic, creating door codes page under checker

--- metroproject/holidays/models.py
+++ metroproject/holidays/models.py
@@ -8,6 +8,9 @@ class Summer(models.Model):
     year_2023 = models.DateField(blank=True, null=True)
     year_2024 = models.DateField(blank=True, null=True)
 
+    def __str__(self):
+        return f'Summer {self.block} {self.year_2021} {self.year_2022} {self.year_2023} {self.year_2024}'
+
 
 class EarlyWinter(models.Model):
     block = models.CharField(max_length=10, default=True, primary_key=True)
@@ -17,6 +20,9 @@ class EarlyWinter(models.Model):
     year_2024 = models.DateField(blank=True, null=True)
     summer = models.ForeignKey(Summer, on_delete=models.CASCADE, related_name='summer_block', default=True)
 
+    def __str__(self):
+        return f'Early {self.block} {self.year_2021} {self.year_2022} {self.year_2023} {self.year_2024}'
+
 class LateWinter(models.Model):
     block = models.OneToOneField(EarlyWinter, on_delete=models.CASCADE, related_name='common_block', primary_key=True)
     year_2021 = models.DateField(blank=True, null=True)
@@ -24,7 +30,13 @@ class LateWinter(models.Model):
     year_2023 = models.DateField(blank=True, null=True)
     year_2024 = models.DateField(blank=True, null=True)
 
+    def __str__(self):
+        return f'Late {self.block} {self.year_2021} {self.year_2022} {self.year_2023} {self.year_2024}'
+
 
 class Driver(models.Model):
     name = models.CharField(max_length=30)
     block = models.ForeignKey(EarlyWinter, on_delete=models.CASCADE, related_name='holiday_block')
+
+    def __str__(self):
+        return f'{self.name} == {self.block}'
--- metroproject/holidays/views.py
+++ metroproject/holidays/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Driver, EarlyWinter, Summer, LateWinter
+from django.db.models import F
 
 # Create your views here.
 def holidays(request):
-    return render(request, 'holidays/holidays.html')
+    chosen_year = request.GET.get('year')
+    if chosen_year is not None:
+        year = 'Our holidays in' + chosen_year.split('_')[1]
+        drivers = Driver.objects.values_list('name', 'block_id', f'block__{chosen_year}', f'block__summer__{chosen_year}',
+                                    f'block__common_block__{chosen_year}')
+    else:
+        year = 'Please choose a year...'
+        drivers = Driver.objects.all()
+    return render(request, 'holidays/holidays.html', context={'drivers': drivers, 'year':year })
