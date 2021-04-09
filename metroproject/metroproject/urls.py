3f9ff5a85c5ab0b792e3100b935c9b27c7ad8492
Marcin Szymanek <szymanek.m@gmail.com>
2021-04-09 18:22:52 +0100

    Revert "added Procfile and requirements.txt"
    This reverts commit 101004dc99242335f53f13dce5ff3496e9d3749b.

--- metroproject/metroproject/urls.py
+++ metroproject/metroproject/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +25,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='checker/index.html')),
     path('holidays/', include('holidays.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
 
